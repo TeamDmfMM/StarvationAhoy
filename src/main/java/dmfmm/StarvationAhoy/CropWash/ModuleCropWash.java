f7b234d6aa03dd096a02b76690a6ca6390d48ce4
mincrmatt12 <matthewmirvish@hotmail.com>
2015-05-23 20:06:03 -0400

    CropWash stubs!
    + Start cropwash
    
    - Remove bad item loader
    
    + Add module

---
+++ src/main/java/dmfmm/StarvationAhoy/CropWash/ModuleCropWash.java
@@ -0,0 +1,27 @@
+package dmfmm.StarvationAhoy.CropWash;
+
+import cpw.mods.fml.relauncher.Side;
+import dmfmm.StarvationAhoy.CropWash.item.CropItemLoader;
+
+/**
+ * Made by mincrmatt12. Do not copy or you will have to face
+ * our legal team (dmf444)
+ */
+public class ModuleCropWash {
+
+    CropItemLoader cropItemLoader;
+
+
+    public void preinit(){
+        cropItemLoader = new CropItemLoader();
+    }
+
+    public void init(Side side) {
+        cropItemLoader.load();
+    }
+
+    public void postinit(){
+
+    }
+
+}
