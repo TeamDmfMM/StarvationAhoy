374d83b1d2f0b165dad0f3d20a51dbd782a09019
dmf444 <becat444@gmail.com>
2016-07-24 22:33:09 -0400

    AutoRoster

--- src/main/java/dmfmm/StarvationAhoy/btmstuff/ModuleBTMStuff.java
+++ src/main/java/dmfmm/StarvationAhoy/btmstuff/ModuleBTMStuff.java
@@ -1,6 +1,8 @@
 package dmfmm.StarvationAhoy.btmstuff;
 
+import dmfmm.StarvationAhoy.btmstuff.blocks.AutomaticRoaster;
 import dmfmm.StarvationAhoy.btmstuff.blocks.SignBlock;
+import dmfmm.StarvationAhoy.btmstuff.te.AutoRoasterTE;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -14,17 +16,22 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class ModuleBTMStuff {
 
     static public ModuleBTMStuff instance = new ModuleBTMStuff();
+    public static Block AutomaticRoaster;
 
     Block signblock;
 
     public void preinit() {
         signblock = new SignBlock();
         signblock.setRegistryName("starvationahoy", "signblock");
+
+        AutomaticRoaster = new AutomaticRoaster();
     }
 
     public void init() {
         GameRegistry.register(signblock);
         GameRegistry.register(new ItemBlock(signblock).setRegistryName(signblock.getRegistryName()));
+
+        GameRegistry.registerTileEntity(AutoRoasterTE.class, "ARTE");
     }
 
 }
---
+++ src/main/java/dmfmm/StarvationAhoy/btmstuff/blocks/AutomaticRoaster.java
@@ -0,0 +1,56 @@
+package dmfmm.StarvationAhoy.btmstuff.blocks;
+
+import dmfmm.StarvationAhoy.Core.SATabs;
+import dmfmm.StarvationAhoy.Meat.ModuleMeat;
+import dmfmm.StarvationAhoy.btmstuff.te.AutoRoasterTE;
+import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+/**
+ * Created by TeamDMFMM on 7/23/2016. Code originally written
+ * for StarvationAhoy. Do not copy without permission,
+ * because that is just mean. Code is VISIBLE SOURCE, therfore
+ * credit us, just don't steal large portions of this.
+ */
+public class AutomaticRoaster extends Block implements ITileEntityProvider{
+
+    public AutomaticRoaster() {
+        super(Material.ANVIL);
+        this.setRegistryName("autoroaster");
+        this.setCreativeTab(SATabs.INSTANCE);
+
+        GameRegistry.register(this);
+        GameRegistry.register(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(World worldIn, int meta) {
+        return new AutoRoasterTE();
+    }
+
+    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
+        if (player.inventory.mainInventory[player.inventory.currentItem] == null) {return false;}
+
+        if (ModuleMeat.registry.isSkinnedItem(player.inventory.mainInventory[player.inventory.currentItem]).value) {
+            if(world.getTileEntity(pos) instanceof AutoRoasterTE){
+                ((AutoRoasterTE)world.getTileEntity(pos)).setEntity(player.inventory.mainInventory[player.inventory.currentItem]);
+                player.addChatComponentMessage(new TextComponentString("Added "+ player.inventory.mainInventory[player.inventory.currentItem].getItem().getUnlocalizedName() +" as Roasting Item"));
+                return true;
+            }
+        }
+        return false;
+    }
+}
